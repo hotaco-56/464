e45f0276a6c0b8886ce645157cbb93325ad05caf
Aiden Lok Ng <ang59@unix1.csc.calpoly.edu>
2026-04-14 05:33:49 -0700

    stupid

---
+++ Trace/TraceForHandin/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "PacketTracer.h"
+
+int main(int argc, char* argv[]) 
+{
+    if (argc < 2) {
+        perror("not enough arguments");
+    }
+
+    PacketTracer tracer;
+    tracer.sniffPackets(argv[1]);
+
+    return 0;
+}
